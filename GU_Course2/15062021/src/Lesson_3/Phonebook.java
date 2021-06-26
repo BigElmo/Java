afb267bd0d6ee3449bd0a1a7bd5531d641de40f5
BigElmo <bigelmogm@gmail.com>
2021-06-26 22:03:46 +1000

    Выполнил п.2
    2. Написать простой класс ТелефонныйСправочник, который хранит в себе список фамилий и телефонных номеров. В этот телефонный справочник с помощью метода add() можно добавлять записи. С помощью метода get() искать номер телефона по фамилии. Следует учесть, что под одной фамилией может быть несколько телефонов, тогда при запросе такой фамилии должны выводиться все телефоны.

--- GU_Course2/15062021/src/Lesson_3/Phonebook.java
+++ GU_Course2/15062021/src/Lesson_3/Phonebook.java
@@ -1,19 +1,33 @@
 package Lesson_3;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Phonebook {
-    private HashMap<String, Long> contacts;
+    private HashMap<String, ArrayList<Long>> contacts;
 
     public Phonebook(int size) {
         contacts = new HashMap<>(size);
     }
 
     public void addPhone(String name, Long phone) {
-        contacts.put(name, phone);
+        if (!contacts.containsKey(name)) {
+            contacts.put(name, new ArrayList<>());
+        }
+        contacts.get(name).add(phone);
     }
 
-    public Long getPhone(String name) {
-        return contacts.getOrDefault(name, 0L);
+    public ArrayList<Long> getPhone(String name) {
+        return contacts.get(name);
+    }
+
+    public void printPhone(String name) {
+        System.out.println(name + ":");
+        if (!contacts.containsKey(name) || contacts.get(name).isEmpty()) {
+            System.out.println("No data");
+        } else {
+            System.out.println(contacts.get(name));
+        }
+        System.out.println();
     }
 }
