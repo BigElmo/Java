29cce2c0b9d9844aef8e3dd2e50939263f570138
BigElmo <bigelmogm@gmail.com>
2021-07-06 20:09:47 +1000

    Доделал отправку из консоли клиента

--- GU_Course2/15062021/src/Lesson_6/Client/MainClient.java
+++ GU_Course2/15062021/src/Lesson_6/Client/MainClient.java
@@ -4,20 +4,40 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class MainClient {
     public static void main(String[] args) {
         final String SERVER_ADDR = "localhost";
         final int SERVER_PORT = 8189;
         Socket socket = null;
+        Scanner sc = new Scanner(System.in);
         try {
             socket = new Socket(SERVER_ADDR, SERVER_PORT);
             System.out.println("Connected");
             DataInputStream in = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
+            Thread readThread = new Thread(()->{
+                try{
+                    while (true){
+                        out.writeUTF(sc.nextLine());
+                    }
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            });
+            readThread.setDaemon(true);
+            readThread.start();
+
             while (true) {
                 String str = in.readUTF();
+                if (str.equals("/exit")) {
+                    System.out.println("Disconnected");
+                    out.writeUTF("/exit");
+                    break;
+                }
                 System.out.println(str);
             }
         } catch (IOException e) {
