19061b12242d6481ed490ed1524221bd26e5835b
BigElmo <bigelmogm@gmail.com>
2021-09-01 23:44:38 +1000

    Task2 done

--- GU_Course3/src/main/java/Main.java
+++ GU_Course3/src/main/java/Main.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Holla!");
-//        1. Написать метод, который меняет два элемента массива местами.(массив может быть любого ссылочного типа);
         String[] strArr = {"A", "B", "C", "D", "E", "F"};
         Integer[] intArr = {1, 2, 3, 4, 5, 6};
         changeElements(strArr, 1, 3);
@@ -10,6 +12,11 @@ public class Main {
         printArr(strArr);
         System.out.println();
         printArr(intArr);
+        System.out.println();
+        ArrayList<String> strList = convertToArrayList(strArr);
+        ArrayList<Integer> intList = convertToArrayList(intArr);
+        System.out.println(strList);
+        System.out.println(intList);
     }
 
     public static <T> void printArr(T[] arr) {
@@ -18,9 +25,16 @@ public class Main {
         }
     }
 
+//    1. Написать метод, который меняет два элемента массива местами.(массив может быть любого ссылочного типа);
     public static <T> void changeElements(T[] arr, int e1, int e2) {
         T temp = arr[e1];
         arr[e1] = arr[e2];
         arr[e2] = temp;
+    }
+//    2. Написать метод, который преобразует массив в ArrayList;
+    public static <T> ArrayList<T> convertToArrayList(T[] arr) {
+        ArrayList<T> arrList = new ArrayList<>();
+        Collections.addAll(arrList, arr);
+        return arrList;
     }
 }
