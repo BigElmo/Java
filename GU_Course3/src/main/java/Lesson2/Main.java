f2c6d8a591554b91046a22cb8b3cbfc19566c781
BigElmo <bigelmogm@gmail.com>
2021-09-06 22:36:27 +1000

    add read query

--- GU_Course3/src/main/java/Lesson2/Main.java
+++ GU_Course3/src/main/java/Lesson2/Main.java
@@ -1,16 +1,25 @@
 package Lesson2;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
-    public static Connection connection;
+
+    private static Connection conn;
+    private static Statement stmt;
 
     public static void main(String[] args) {
         System.out.println("Ciao!");
         try {
             connect();
+            ResultSet rs = stmt.executeQuery("SELECT name, surname FROM teachers WHERE id = 2;");
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columns = rsmd.getColumnCount();
+            while (rs.next()) {
+                for (int i = 1; i <= columns; i++) {
+                    System.out.print(rs.getString(i) + " ");
+                }
+                System.out.println();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -18,14 +27,23 @@ public class Main {
         }
     }
 
-    public static void connect() throws SQLException {
-        connection = DriverManager.getConnection("jdbc:sqlite:teachers.db");
+    public static void connect() throws SQLException, ClassNotFoundException {
+        Class.forName("org.sqlite.JDBC");
+        conn = DriverManager.getConnection("jdbc:sqlite:teachers.db");
+        stmt = conn.createStatement();
     }
 
     public static void disconnect() {
-        if (connection != null) {
+        if (stmt != null) {
             try {
-                connection.close();
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
             } catch (SQLException e) {
                 e.printStackTrace();
             }
