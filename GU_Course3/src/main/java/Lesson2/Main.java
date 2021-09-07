a3cbdce76562fffff009c8662bb890c77a32eb24
BigElmo <bigelmogm@gmail.com>
2021-09-07 20:38:02 +1000

    Course3_homework2 done

--- GU_Course3/src/main/java/Lesson2/Main.java
+++ GU_Course3/src/main/java/Lesson2/Main.java
@@ -11,20 +11,55 @@ public class Main {
         System.out.println("Ciao!");
         try {
             connect();
-            ResultSet rs = stmt.executeQuery("SELECT name, surname FROM teachers WHERE id = 2;");
-            ResultSetMetaData rsmd = rs.getMetaData();
-            int columns = rsmd.getColumnCount();
-            while (rs.next()) {
-                for (int i = 1; i <= columns; i++) {
-                    System.out.print(rs.getString(i) + " ");
-                }
-                System.out.println();
-            }
+            createTableEx();
+            insertEx();
+            insertEx();
+            insertEx();
+            readEx();
+            System.out.println();
+            deleteEx();
+            readEx();
+            dropEx();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             disconnect();
         }
+    }
+
+    public static void createTableEx() throws SQLException {
+        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS example (\n" +
+                "    id      INTEGER PRIMARY KEY AUTOINCREMENT\n" +
+                "                    UNIQUE\n" +
+                "                    NOT NULL,\n" +
+                "    Name    TEXT    NOT NULL,\n" +
+                "    Surname TEXT    NOT NULL,\n" +
+                "    Age     INTEGER\n" +
+                ");");
+    }
+
+    public static void insertEx() throws SQLException {
+        stmt.executeUpdate("INSERT INTO example (Name, Surname) VALUES ('Bob', 'Marley');");
+    }
+
+    public static void readEx() throws SQLException {
+        ResultSet rs = stmt.executeQuery("SELECT * FROM example;");
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columns = rsmd.getColumnCount();
+        while (rs.next()) {
+            for (int i = 1; i <= columns; i++) {
+                System.out.print(rs.getString(i) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void deleteEx() throws SQLException {
+        stmt.executeUpdate("DELETE FROM example WHERE id = 1;");
+    }
+
+    public static void dropEx() throws SQLException {
+        stmt.executeUpdate("DROP TABLE IF EXISTS example;");
     }
 
     public static void connect() throws SQLException, ClassNotFoundException {
