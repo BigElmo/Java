98dd384e48225b86e1a3fc56cc097250e2464e9f
BigElmo <bigelmogm@gmail.com>
2021-09-10 13:04:10 +1000

    Task1 done

--- GU_Course3/src/main/java/Lesson3/Main.java
+++ GU_Course3/src/main/java/Lesson3/Main.java
@@ -7,14 +7,27 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("NiHao!");
         Random random = new Random();
-        File file = new File("Project_files/Lesson3_files/file1.txt");
-        file.mkdirs();
-        byte[] barr = new byte[50];
-        random.nextBytes(barr);
-//        try (OutputStream out = new BufferedOutputStream(new FileOutputStream(file))) {
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        File file = new File("file1.txt");
+        byte[] random_barr = new byte[50];
+        random.nextBytes(random_barr);
+
+        try (OutputStream out = new BufferedOutputStream(new FileOutputStream(file))) {
+            out.write(random_barr);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        byte[] task1_barr = new byte[50];
+        try (FileInputStream in = new FileInputStream(file)) {
+            int count;
+            while ((count = in.read(task1_barr)) > 0) {
+                for (int i = 0; i < count; i++) {
+                    System.out.print(task1_barr[i] + " ");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
