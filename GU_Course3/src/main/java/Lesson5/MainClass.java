aa159b5cf867668ede6c4d1beb95290fefbdb7c0
BigElmo <bigelmogm@gmail.com>
2021-09-18 23:46:04 +1000

    add semaphore for tunnel. Task1 done

--- GU_Course3/src/main/java/Lesson5/MainClass.java
+++ GU_Course3/src/main/java/Lesson5/MainClass.java
@@ -3,11 +3,13 @@ package Lesson5;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
 
 public class MainClass {
     public static final int CARS_COUNT = 4;
     public static CyclicBarrier cyclicBarrier;
     public static CountDownLatch cdl;
+    public static Semaphore semaphore;
 
     public static void main(String[] args) throws BrokenBarrierException, InterruptedException {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
@@ -15,6 +17,7 @@ public class MainClass {
         Car[] cars = new Car[CARS_COUNT];
         cyclicBarrier = new CyclicBarrier(CARS_COUNT + 1); //все участники и один поток, ожидающий объявления старта
         cdl = new CountDownLatch(CARS_COUNT);
+        semaphore = new Semaphore(CARS_COUNT / 2);
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
         }
--- GU_Course3/src/main/java/Lesson5/Tunnel.java
+++ GU_Course3/src/main/java/Lesson5/Tunnel.java
@@ -10,12 +10,14 @@ public class Tunnel extends Stage {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                MainClass.semaphore.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                MainClass.semaphore.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
