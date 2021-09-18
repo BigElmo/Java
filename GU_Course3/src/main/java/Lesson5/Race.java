680a4058a96f22c26dae6f706c4c73ece16aaf91
BigElmo <bigelmogm@gmail.com>
2021-09-18 12:50:21 +1000

    Add classes from task1

---
+++ GU_Course3/src/main/java/Lesson5/Race.java
@@ -0,0 +1,12 @@
+package Lesson5;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Race {
+    private ArrayList<Stage> stages;
+    public ArrayList<Stage> getStages() { return stages; }
+    public Race(Stage... stages) {
+        this.stages = new ArrayList<>(Arrays.asList(stages));
+    }
+}
---
+++ GU_Course3/src/main/java/Lesson5/Stage.java
@@ -0,0 +1,10 @@
+package Lesson5;
+
+public abstract class Stage {
+    protected int length;
+    protected String description;
+    public String getDescription() {
+        return description;
+    }
+    public abstract void go(Car c);
+}
